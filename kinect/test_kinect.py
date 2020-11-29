6c6f3fb69d1b6e31f8cded9573dc46c5e6fba0a5
Kueakun <kueakun.liwc@gmail.com>
2020-11-29 16:11:06 +0700

    Get realworld distance data

--- kinect/test_kinect.py
+++ kinect/test_kinect.py
@@ -14,16 +14,27 @@ def get_depth():
     array,_ = freenect.sync_get_depth()
     array = array.astype(np.uint8)
     return array
+
+def get_real_depth():
+    array,_ = freenect.sync_get_depth()
+    return array
+
+def get_real_dist(pixel):
+    #THIS IS FUCKING MAGIC WE DON'T KNOW WHAT IT IS
+    return 0.1236 * np.tan(pixel / 2842.5 + 1.1863)
+
  
 if __name__ == "__main__":
     while 1:
         #get a frame from RGB camera
-        frame = get_video()
+        # frame = get_video()
         #get a frame from depth sensor
         depth = get_depth()
         #display RGB image
-        cv2.imshow('RGB image',frame)
+        # cv2.imshow('RGB image',frame)
         #display depth image
+        depth = cv2.circle(depth, (320,240), 3, (0,255,0), -1)
+
         cv2.imshow('Depth image',depth)
  
         # quit program when 'esc' key is pressed
@@ -32,5 +43,9 @@ if __name__ == "__main__":
             break
         elif k == ord('a'):
             array,_ = freenect.sync_get_depth()
-            print(array[0])
+            # array, = freenect.get_depth()
+            rawDisparity = array[240][320]
+            print("YAYY", get_real_dist(rawDisparity))
+            # print(array.shape)
+            # print(array[0])
     cv2.destroyAllWindows()
